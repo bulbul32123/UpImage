cd88a42d03f68a52da6e298ef9c9dbfcde249d02
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-10-11 16:32:26 +0600

    lib axios updated

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,9 +1,8 @@
-// src/lib/axios.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api", // same domain as your Next.js app
-  withCredentials: true, // ✅ ensures cookies are sent/received
+  baseURL: "/api", 
+  withCredentials: true, 
 });
 
 export default api;
