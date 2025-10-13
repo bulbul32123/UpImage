fcf45de6b71d9095cbfc49a7f3ce656f59bcaf2c
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-10-13 17:46:02 +0600

    data.js added

---
+++ src/utils/data.js
@@ -0,0 +1,22 @@
+export const tabs = [
+    {
+        id: "",
+        label: "Account Details",
+        icon: "User",
+    },
+    {
+        id: "subscription",
+        label: "Subscription & Billing",
+        icon: "CreditCard",
+    },
+    {
+        id: "user-analytics",
+        label: "Usage Analytics",
+        icon: "BarChart3",
+    },
+    {
+        id: "security",
+        label: "Security Settings",
+        icon: "Shield",
+    },
+];
