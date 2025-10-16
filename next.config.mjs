b32dc638536b11114cedc2cf1bbf8de176e138ba
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-10-16 17:54:16 +0600

    app updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['https://res.cloudinary.com/'],
-      },      
+        domains: ['res.cloudinary.com'],
+        
+    },
     api: {
         bodyParser: {
             sizeLimit: '10mb',
